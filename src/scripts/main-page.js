56acb558576126282ee2fb735cdc4cc150dfaa2c
unktir <valera47304@gmail.com>
2024-04-28 21:25:52 +0500

    refactor(main-page): change the name of the script file and move all scripts there

---
+++ src/scripts/main-page.js
@@ -0,0 +1,19 @@
+import "./carousel.js";
+
+document.getElementById("contact_form").addEventListener("submit", (e) => {
+  e.preventDefault();
+  
+  let name = document.getElementById("name").value;
+  let phone_number = document.getElementById("phone_number").value;
+  let email = document.getElementById("email").value;
+  let interested_in = document.getElementById("interested_in").value;
+  let message = document.getElementById("message").value;
+
+  let alerttext;
+  if (name === "" && interested_in === "") {
+      alerttext = `Вы отправили запрос!`;
+  } else {
+      alerttext = `${name}, вы отправили запрос по теме: ${interested_in}!`;
+  }
+  alert(alerttext);
+})
