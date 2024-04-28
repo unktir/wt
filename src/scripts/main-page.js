8cad1d14dbee72c3a9424dd86d42ecbe8ba60816
unktir <valera47304@gmail.com>
2024-04-28 21:28:48 +0500

    feat(main-page): add the modal of sended form to the main page

--- src/scripts/main-page.js
+++ src/scripts/main-page.js
@@ -1,19 +1,31 @@
 import "./carousel.js";
 
-document.getElementById("contact_form").addEventListener("submit", (e) => {
-  e.preventDefault();
-  
-  let name = document.getElementById("name").value;
-  let phone_number = document.getElementById("phone_number").value;
-  let email = document.getElementById("email").value;
-  let interested_in = document.getElementById("interested_in").value;
-  let message = document.getElementById("message").value;
+let modal_form_sended = document.getElementById("modal-form-sended");
 
-  let alerttext;
-  if (name === "" && interested_in === "") {
-      alerttext = `Вы отправили запрос!`;
-  } else {
-      alerttext = `${name}, вы отправили запрос по теме: ${interested_in}!`;
-  }
-  alert(alerttext);
+let close_modal_form_sended_button = document.getElementById("close-modal-form-sended");
+
+document.getElementById("contact_form").addEventListener("submit", (e) => {
+    e.preventDefault();
+    
+    let name = document.getElementById("name").value;
+    let phone_number = document.getElementById("phone_number").value;
+    let email = document.getElementById("email").value;
+    let interested_in = document.getElementById("interested_in").value;
+    let message = document.getElementById("message").value;
+
+    modal_form_sended.style.display = "flex";
 })
+
+close_modal_form_sended_button.onclick = function() {
+    modal_form_sended.style.display = "none";
+}
+
+window.onclick = function(event) {
+  if (event.target == modal_form_sended) {
+    modal_form_sended.style.display = "none";
+  }
+}
+
+document.getElementById("go-back").addEventListener("click", () => {
+    history.back();
+  });
