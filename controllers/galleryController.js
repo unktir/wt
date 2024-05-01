133aaa2e56e33c3d7c9772df4a990fc1fdc46eb0
unktir <valera47304@gmail.com>
2024-05-01 17:02:02 +0500

    fix(gallery-page): fix carousel

--- controllers/galleryController.js
+++ controllers/galleryController.js
@@ -1,40 +1,48 @@
 const gallery_page = {
-    project_images: [
+    carousel_items: [
         {
-            src: 'images/gallery-page/carousel/image_1.png',
-            alt: 'Image 1'
-        },
-        {
-            src: 'images/gallery-page/carousel/image_2.png',
-            alt: 'Image 2'
-        },
-        {
-            src: 'images/gallery-page/carousel/image_3.png',
-            alt: 'Image 3'
-        },
-        {
-            src: 'images/gallery-page/carousel/image_4.png',
-            alt: 'Image 4'
-        },
-        {
-            src: 'images/gallery-page/carousel/image_5.png',
-            alt: 'Image 5'
-        },
-        {
-            src: 'images/gallery-page/carousel/image_6.png',
-            alt: 'Image 6'
-        },
-        {
-            src: 'images/gallery-page/carousel/image_7.png',
-            alt: 'Image 7'
-        },
-        {
-            src: 'images/gallery-page/carousel/image_8.png',
-            alt: 'Image 8'
-        },
-        {
-            src: 'images/gallery-page/carousel/image_9.png',
-            alt: 'Image 9'
+            project_images: [
+                {
+                    src: '',
+                    alt: 'No image'
+                },
+                {
+                    src: 'images/gallery-page/carousel/image_1.png',
+                    alt: 'Image 1'
+                },
+                {
+                    src: 'images/gallery-page/carousel/image_2.png',
+                    alt: 'Image 2'
+                },
+                {
+                    src: 'images/gallery-page/carousel/image_3.png',
+                    alt: 'Image 3'
+                },
+                {
+                    src: 'images/gallery-page/carousel/image_4.png',
+                    alt: 'Image 4'
+                },
+                {
+                    src: 'images/gallery-page/carousel/image_5.png',
+                    alt: 'Image 5'
+                },
+                {
+                    src: 'images/gallery-page/carousel/image_6.png',
+                    alt: 'Image 6'
+                },
+                {
+                    src: 'images/gallery-page/carousel/image_7.png',
+                    alt: 'Image 7'
+                },
+                {
+                    src: 'images/gallery-page/carousel/image_8.png',
+                    alt: 'Image 8'
+                },
+                {
+                    src: 'images/gallery-page/carousel/image_9.png',
+                    alt: 'Image 9'
+                }
+            ]
         }
     ]
 };
