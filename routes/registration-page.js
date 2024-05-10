7eab5fd17d68ef53cf4591c4e5b423a412fcbb2e
unktir <valera47304@gmail.com>
2024-05-10 14:29:52 +0500

    feat(registration-page): add the registration page

--- routes/registration-page.js
+++ routes/registration-page.js
@@ -6,22 +6,15 @@ var register_page = require('../controllers/registerController');
 
 var loginsModel = require("../models/loginsModel");
 
-// router.get('/', function(req, res) {
-//     res.render('register', { 
-//       title: 'Whitesquare',
-//       pname: 'AUTH',
-//       navmenu: navmenu });
-// });
-
 var data = {
     title: 'Digital Project',
-    page_name: 'register-page',
+    page_name: 'registration-page',
     layout,
     register_page
 };
   
 router.get('/', function(req, res, next) {
-    res.render('register-page', data);
+    res.render('registration-page', data);
 });
 
 router.post('/', loginsModel.register_user);
